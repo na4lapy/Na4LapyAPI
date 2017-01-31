19d5eaaa13d416f53de3efe0c46db0a3b162dbad
Andrzej Butkiewicz <scoot_pl@me.com>
2017-01-31 21:44:04 +0100

    Podniesienie wersji dla obsługi sesji

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     dependencies: [
             .Package(url: "https://github.com/IBM-Swift/Kitura-Session.git", majorVersion: 1, minor: 4),
             .Package(url: "https://github.com/IBM-Swift/Kitura-CORS.git", majorVersion: 1, minor: 4),
-            .Package(url: "https://github.com/na4lapy/na4lapy-api-core", majorVersion: 0, minor: 2)
+            .Package(url: "https://github.com/na4lapy/na4lapy-api-core", majorVersion: 0, minor: 3)
      ]
 )
 
