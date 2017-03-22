058900909b0d399d6e3dcdbec9710e4ef54ed56d
Andrzej Butkiewicz <scoot_pl@me.com>
2017-03-22 20:17:34 +0100

    secure cookie

--- Sources/Server/main.swift
+++ Sources/Server/main.swift
@@ -26,7 +26,7 @@ var listenPort: Int = defaultListenPort
 
 // Session
 //
-let session = Session(secret: UUID().uuidString)
+let session = Session(secret: UUID().uuidString, cookie: [.secure(true)])
 
 // CORS
 //
